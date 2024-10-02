7799d6e89060c9bf025b3e927c784a83b686b7e4
kasia.kordys <kasia.kordys@gmail.com>
2024-10-02 16:43:18 +0300

    use contact hook to fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import "./App.css";
+import { useGetContactsQuery } from "./services/contactsApi";
 
 function App() {
+  const { data, error, isLoading, isFetching, isSuccess } =
+    useGetContactsQuery();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <div>
+        <h1>React Redux RTK Query in use</h1>
+        {isLoading && <h2>...Loading</h2>}
+        {isFetching && <h2>...Fetching</h2>}
+        {error && <h2>Something went wrong</h2>}
+        {isSuccess && (
+          <div>
+            {data?.map((contact) => {
+              return (
+                <div key={contact.id}>
+                  <div>{contact.name}</div>
+                  <div>{contact.name}</div>
+                  <div>{contact.age}</div>
+                  <div>{contact.company}</div>
+                  <div>{contact.gender}</div>
+                  <div>{contact.email}</div>
+                  <img alt="face" src={contact.photo} />
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
